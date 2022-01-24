4d24c8ebb6b8c23345176106ba376111ff089b8a
FIZIS-net <pshift4180@gmail.com>
2022-01-24 18:02:31 +0300

    add joiner function

--- main.py
+++ main.py
@@ -1,77 +1,85 @@
+from urllib import request
 from spamBot import spamBot
 import requests as r
 import random
 import time
+import json
 
 id_copied_chat = input(f'Введите id чата откуда копировать сообщения: ')
 id_insert_chat = input(f'Введите id чата куда копировать сообщения: ')
-messages_count = int(input(f'Введите число сообщений кратное 100: '))
-delay_start = int(input(f'Введите минимальную задержку на сервере: '))
-delay_end = int(input(f'Введите максимальную задержку: '))
-response_delay = int(input(f'Введите задержку для ответа на предыдущее сообщение: '))
+# messages_count = int(input(f'Введите число сообщений кратное 100: '))
+# delay_start = int(input(f'Введите минимальную задержку на сервере: '))
+# delay_end = int(input(f'Введите максимальную задержку: '))
+# response_delay = int(input(f'Введите задержку для ответа на предыдущее сообщение: '))
 
 
 bot = spamBot(id_copied_chat, id_insert_chat)
 tokens_list = open('tokens.txt', 'r', encoding='utf-8').read().splitlines()
 
-firs_session = bot.create_session(tokens_list[0])
+firs_session = bot.create_session(tokens_list[1])
 
-loaded_msg = bot.get_messages(firs_session)
+# loaded_msg = bot.get_messages(firs_session)
+req = bot.send_join(firs_session, "2PAkmUdY")
+print(req.text)
+# with open('messages.json', 'w') as outfile:
+#     json.dump(loaded_msg, outfile)
 
-all_messages = {}
-all_messages.update(loaded_msg)
-for i in range(0, (messages_count//100)):
-    loaded_msg = bot.get_messages_before(firs_session, bot.get_last_messages_id(all_messages))
-    all_messages.update(loaded_msg)
+# print(loaded_msg)
 
-users_ids = []
-for message in all_messages:
-    if all_messages[message]['author_id'] not in  users_ids:
-        users_ids.append(all_messages[message]['author_id'])
+# all_messages = {}
+# all_messages.update(loaded_msg)
+# for i in range(0, (messages_count//100)):
+#     loaded_msg = bot.get_messages_before(firs_session, bot.get_last_messages_id(all_messages))
+#     all_messages.update(loaded_msg)
 
-random.shuffle(users_ids)
+# users_ids = []
+# for message in all_messages:
+#     if all_messages[message]['author_id'] not in  users_ids:
+#         users_ids.append(all_messages[message]['author_id'])
 
-sessions = {}
-sessions_count = 0
-for token in tokens_list:
-    sessions[sessions_count] = {"token": bot.create_session(token), "users_ids": []}
-    sessions_count += 1
+# random.shuffle(users_ids)
 
-session_id = 0
-for ids in users_ids:
-    sessions[session_id]['users_ids'].append(ids)
-    session_id += 1
-    if session_id == (len(sessions)):
-        session_id = 0
+# sessions = {}
+# sessions_count = 0
+# for token in tokens_list:
+#     sessions[sessions_count] = {"token": bot.create_session(token), "users_ids": []}
+#     sessions_count += 1
+
+# session_id = 0
+# for ids in users_ids:
+#     sessions[session_id]['users_ids'].append(ids)
+#     session_id += 1
+#     if session_id == (len(sessions)):
+#         session_id = 0
 
 
-loaded_msg = bot.reverseDict(all_messages)
-total_sent = 0
-previous_sender = 0
-for key in loaded_msg:
-    previous_sender = loaded_msg[key]["author_id"]
-    if loaded_msg[key]["author_id"] != previous_sender:
-        delay_active = True
-    else:
-        delay_active = False
-    try:
-        msg= loaded_msg[key]["message_content"]
-        print(f'Sending message {msg}')
-        _data = {'content':msg, 'tts':False}
-        delay = random.randint(delay_start, delay_end)
-        if('https://' not in msg and ':' not in msg and '$' not in msg):
-            for session in sessions:
-                if loaded_msg[key]["author_id"] in sessions[session]['users_ids']:
-                    bot.send_message(sessions[session]['token'], msg)
-                    total_sent += 1
-                    previous_sender = loaded_msg[key]["author_id"]
-        if(delay_active):
-            print(f'Сообщение успешно отправлено (всего отправлено {total_sent}).')
-            print(f'Перерыв {delay} секунд')
-            time.sleep(delay)
-        else:
-            print(f'отвечает другой чеовек, задержка не нужна (всего отправлено {total_sent})')
-            time.sleep(response_delay)
-    except Exception as e:
-        print(f'Что-то пошло не так ERROR: {e.with_traceback}')
-        time.sleep(20)
+# loaded_msg = bot.reverseDict(all_messages)
+# total_sent = 0
+# previous_sender = 0
+# for key in loaded_msg:
+#     previous_sender = loaded_msg[key]["author_id"]
+#     if loaded_msg[key]["author_id"] != previous_sender:
+#         delay_active = True
+#     else:
+#         delay_active = False
+#     try:
+#         msg= loaded_msg[key]["message_content"]
+#         print(f'Sending message {msg}')
+#         _data = {'content':msg, 'tts':False}
+#         delay = random.randint(delay_start, delay_end)
+#         if('https://' not in msg and ':' not in msg and '$' not in msg):
+#             for session in sessions:
+#                 if loaded_msg[key]["author_id"] in sessions[session]['users_ids']:
+#                     bot.send_message(sessions[session]['token'], msg)
+#                     total_sent += 1
+#                     previous_sender = loaded_msg[key]["author_id"]
+#         if(delay_active):
+#             print(f'Сообщение успешно отправлено (всего отправлено {total_sent}).')
+#             print(f'Перерыв {delay} секунд')
+#             time.sleep(delay)
+#         else:
+#             print(f'отвечает другой чеовек, задержка не нужна (всего отправлено {total_sent})')
+#             time.sleep(response_delay)
+#     except Exception as e:
+#         print(f'Что-то пошло не так ERROR: {e.with_traceback}')
+#         time.sleep(20)
--- spamBot.py
+++ spamBot.py
@@ -1,4 +1,6 @@
+import json
 import time
+from urllib import request, response
 import requests as r
 from collections import OrderedDict, deque
 
@@ -18,8 +20,10 @@ class spamBot():
         """        
         msgArr = {}
         messages_ids=0
-
-        copied_messages = session.get(f'https://discord.com/api/v9/channels/{ self.id_copied_chat }/messages?limit=100').json()
+        try:
+            copied_messages = session.get(f'https://discord.com/api/v9/channels/{ self.id_copied_chat }/messages?limit=100').json()
+        except Exception as e:
+            print(session.get(f'https://discord.com/api/v9/channels/{ self.id_copied_chat }/messages?limit=100').text)
 
         for message in copied_messages:
             try:
@@ -41,6 +45,25 @@ class spamBot():
         """        
         session = r.session()
         session.headers['authorization'] = discord_token
+        session.headers['accept'] = "*/*"
+        session.headers["connection"]=  "keep-alive"
+        session.headers["accept-encoding"] = "br"
+        session.headers["accept-language"] = "en-GB"
+        session.headers["content-type"] = "application/json"
+        session.headers["X-Debug-Options"] = "bugReporterEnabled"
+        session.headers["cache-control"] = "no-cache"
+        session.headers["sec-ch-ua"] = "'Chromium';v='92', ' Not A;Brand';v='99', 'Google Chrome';v='92'"
+        session.headers["sec-fetch-site"] = "same-origin"
+        session.headers["x-context-properties"] = "eyJsb2NhdGlvbiI6IkpvaW4gR3VpbGQiLCJsb2NhdGlvbl9ndWlsZF9pZCI6Ijg4NTkwNzE3MjMwNTgwOTUxOSIsImxvY2F0aW9uX2NoYW5uZWxfaWQiOiI4ODU5MDcxNzIzMDU4MDk1MjUiLCJsb2NhdGlvbl9jaGFubmVsX3R5cGUiOjB9"
+        session.headers["x-fingerprint"] = self.get_fingerprint()
+        session.headers["x-super-properties"] = "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiRmlyZWZveCIsImRldmljZSI6IiIsInN5c3RlbV9sb2NhbGUiOiJlbi1VUyIsImJyb3dzZXJfdXNlcl9hZ2VudCI6Ik1vemlsbGEvNS4wIChXaW5kb3dzIE5UIDEwLjA7IFdpbjY0OyB4NjQ7IHJ2OjkzLjApIEdlY2tvLzIwMTAwMTAxIEZpcmVmb3gvOTMuMCIsImJyb3dzZXJfdmVyc2lvbiI6IjkzLjAiLCJvc192ZXJzaW9uIjoiMTAiLCJyZWZlcnJlciI6IiIsInJlZmVycmluZ19kb21haW4iOiIiLCJyZWZlcnJlcl9jdXJyZW50IjoiIiwicmVmZXJyaW5nX2RvbWFpbl9jdXJyZW50IjoiIiwicmVsZWFzZV9jaGFubmVsIjoic3RhYmxlIiwiY2xpZW50X2J1aWxkX251bWJlciI6MTAwODA0LCJjbGllbnRfZXZlbnRfc291cmNlIjpudWxsfQ=="
+        session.headers["sec-fetch-dest"] = "empty"
+        session.headers["sec-fetch-mode"] = "cors"
+        session.headers["sec-fetch-site"] = "same-origin"
+        session.headers["origin"] = "https://discord.com"
+        session.headers["referer"] = "https://discord.com/channels/@me"
+        session.headers["user-agent"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) discord/0.0.16 Chrome/91.0.4472.164 Electron/13.4.0 Safari/537.36"
+        session.headers["te"] =  "trailers"
         return session
 
     def get_messages_before(self, session, last_message_id):
@@ -107,3 +130,36 @@ class spamBot():
         except Exception as e:
             return e
 
+    def send_Typing(self, session):
+        response = session.post(f'https://discord.com/api/v9/channels/{self.id_insert_chat}/typing')
+        return response
+
+    def get_cookie(self):
+        resp = r.get(f"https://discord.com")
+        CookieDict = {}
+        for cookie in resp.cookies:
+            if cookie.name == "__dcfduid":
+                CookieDict["Dcfduid"] = cookie.value
+            if cookie.name == "__sdcfduid":
+                CookieDict["Sdcfduid"] = cookie.value
+        return CookieDict
+
+    def send_join(self, session, inviteCode):
+        url = "https://discord.com/api/v9/invites/" + inviteCode
+        Cookie = self.get_cookie()
+        print(Cookie)
+        if Cookie["Dcfduid"] == "" and Cookie["Sdcfduid"] == "":
+            print("Error: Cookie is empty")
+            return
+        Cookies = "__dcfduid=" + Cookie["Dcfduid"] + "; " + "__sdcfduid=" + Cookie["Sdcfduid"] + "; " + "locale=ru"
+        session.headers["cookie"] = Cookies
+        req = session.post(url , data="{}")
+        return req
+
+
+    def get_fingerprint(self):
+        resp = r.get(f"https://discordapp.com/api/v9/experiments")
+        resp = json.loads(resp.text)
+        resp = resp["fingerprint"]
+        print(resp)
+        return resp
